ea3706ad034be84a43a8dffbd5d3f8bcd06b87a8
Pulasthi Bandara <pulasthi1989@gmail.com>
2022-12-07 12:24:46 +1300

    documentation links

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
-/**
- * @since 0.1.0
- */
+import * as stream from './stream'
 
-export * as stream from './stream'
+export {
+  /**
+   * Exports the streams implementation as named object so that
+   * it could be used with IDEs that support smart imports.
+   *
+   * Implementation: {@link stream.Stream}.
+   *
+   * @example
+   * import { stream } from 'fp-async-generator-streams'
+   *
+   * @since 0.1.0
+   */
+  stream
+}
